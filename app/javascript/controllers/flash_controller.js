69550f585433e0a42e8a824818277c548f0f4e0d
guimelo03 <guilhermemelo03@hotmail.com>
2026-04-26 14:50:45 -0300

    refactor: scss folder organization & client_avatar

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -2,11 +2,18 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
     connect() {
+        this.element.classList.add("flash-enter")
+
+        requestAnimationFrame(() => {
+            this.element.classList.add("flash-enter-active")
+        })
+
         setTimeout(() => {
             this.element.classList.add("fade-out")
+
             setTimeout(() => {
                 this.element.remove()
-            }, 300)
+            }, 400)
         }, 3000)
     }
 }
