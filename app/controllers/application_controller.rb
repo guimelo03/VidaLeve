c4d04f17052238db1992501e74e62c85f88e6d6d
guimelo03 <guilhermemelo03@hotmail.com>
2026-04-26 19:47:14 -0300

    remove: deprecated method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,16 +12,4 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource_or_scope)
     root_path
   end
-
-  private
-
-  def ensure_onboarding_completed
-    return unless user_signed_in?
-    return if current_user.has_complete_info?
-    return if controller_name == "users" && [ "onboarding", "update_onboarding" ].include?(action_name)
-
-    return if devise_controller?
-
-    redirect_to onboarding_path, alert: "Complete seu perfil para continuar"
-  end
 end
